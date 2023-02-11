a7d0e174b2502420b014e645af76cd1b851ba40d
DioTri26 <123969006+DioTri26@users.noreply.github.com>
2023-02-11 22:38:57 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Manager from './Manager';
+import {View, Text, Button} from 'react-native';
+import Info from './Src/Screen/info';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const Stack = createNativeStackNavigator();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Home = (props) => {
+    const navigation = props.navigation;
+    return (<View>
+        <Text>TRANG CHU</Text>
+        <Button
+            title='Thông tin cá nhân'
+            onPress={() => navigation.navigate('Info', {name: 'Tri'})}
+        />
+    </View>);
+};
+
+const App = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name='Home' component={Home} />
+                <Stack.Screen name='Manager' component={Manager} /> 
+                <Stack.Screen name='Info' component={Info} />
+
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+export default App;
